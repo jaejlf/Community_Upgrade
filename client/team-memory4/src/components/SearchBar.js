f3a159e7ac6fc4870ad00c8c9fcdf089611eba50
Woojung0618 <woojung0618@gmail.com>
2022-03-09 02:23:45 +0900

    Search Bar Styling

--- client/team-memory4/src/components/SearchBar.js
+++ client/team-memory4/src/components/SearchBar.js
@@ -1,17 +1,38 @@
 import React, { useState, useContext, useEffect } from "react";
 import Search from '../assets/images/search.png';
+import '../styles/Board.css';
 
 
 const SearchBar = () => {
+    const [option, setOption] = useState("title");  // title, content, writer
+
     return (
-        <div>
-            <img src={Search} />
-            <input 
-                className="searchbar"/>
+        <div className="searchbar">
+            <img src={Search} className="search-img" />
+            <input
+                className="search-input" />
             <div className="search-options">
-                <button>글제목</button>
-                <button>글내용</button>
-                <button>작성자</button>
+                <button
+                    className={
+                        option === "title" ? 
+                        'search-option-select-o' : 'search-option-select-x'
+                    }
+                    onClick={() => setOption("title")}
+                >글제목</button>
+                <button
+                    className={
+                        option === "content" ? 
+                        'search-option-select-o' : 'search-option-select-x'
+                    }
+                    onClick={() => setOption("content")}
+                >글내용</button>
+                <button
+                    className={
+                        option === "writer" ? 
+                        'search-option-select-o' : 'search-option-select-x'
+                    }
+                    onClick={() => setOption("writer")}
+                >작성자</button>
             </div>
         </div>
     )
