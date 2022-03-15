4d8152bdf3d34ce5addb08d408d011ea0c207cc5
Woojung0618 <woojung0618@gmail.com>
2022-03-16 01:38:31 +0900

    대댓글 api 연결

--- client/team-memory4/src/components/MyComment.js
+++ client/team-memory4/src/components/MyComment.js
@@ -9,7 +9,7 @@ import '../styles/Comments.css';
 const MyComment = ({ props }) => {
     const authContext = useContext(AuthContext);
     const recommentContext = useContext(RecommentContext);
-    const [recomment, setRecomment] = useState(""); // 답글 관련 api 에 넘길 것들 세팅하기
+    const [recomment, setRecomment] = useState("");
     const [myComment, setMyComment] = useState("");
     let postId = props * 1;
 
@@ -23,29 +23,53 @@ const MyComment = ({ props }) => {
 
     const commentWriteHandler = async () => {
         console.log(myComment);
-        await postApi(
-            {
-                content: myComment,
-            },
-            `/comment/${postId}`,
-            authContext.state.token
-        )
-        .then(({ status, data }) => {
-            if (status === 200) {
-                console.log(status, data);
-                window.location.replace(`/post/${postId}`);  // 새로고침하여 댓글 다시 불러오기
-            } else if (status === 500) {
-                alert("게시글 등록 시 오류가 발생했습니다.");
-            } else if (status === 400) {
-                alert("내용을 입력해주세요.");
-            } else if (status === 501) {
-                alert("로그인을 해야 댓글을 작성할 수 있습니다.");
-            }
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-
+        if (recommentContext.state.recommentId === null) {  // 댓글 작성 (대댓글 X)
+            await postApi(
+                {
+                    content: myComment,
+                },
+                `/comment/${postId}`,
+                authContext.state.token
+            )
+            .then(({ status, data }) => {
+                if (status === 200) {
+                    console.log('댓글 작성 post api', status);
+                    window.location.replace(`/post/${postId}`);  // 새로고침하여 댓글 다시 불러오기
+                } else if (status === 500) {
+                    alert("댓글 등록 시 오류가 발생했습니다.");
+                } else if (status === 400) {
+                    alert("내용을 입력해주세요.");
+                } else if (status === 501) {
+                    alert("로그인을 해야 댓글을 작성할 수 있습니다.");
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+        } else { // 대댓글 작성
+            await postApi(
+                {
+                    content: myComment,
+                },
+                `/comment/${recommentContext.state.recommentId}`,
+                authContext.state.token
+            )
+            .then(({ status, data }) => {
+                if (status === 200) {
+                    console.log('대댓글 post api', status);
+                    window.location.replace(`/post/${postId}`);  // 새로고침하여 댓글 다시 불러오기
+                } else if (status === 500) {
+                    alert("댓글 등록 시 오류가 발생했습니다.");
+                } else if (status === 400) {
+                    alert("내용을 입력해주세요.");
+                } else if (status === 501) {
+                    alert("로그인을 해야 댓글을 작성할 수 있습니다.");
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+        }
     }
 
     return (
